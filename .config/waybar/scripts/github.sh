7690687862bc42e3c144c70c74b785770b817c2c
ayusshrathore <ayusshrathore@gmail.com>
2023-01-03 16:35:31 +0530

    chore: update waybar config

--- .config/waybar/scripts/github.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-count=`curl -u ayusshrathore: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 https://api.github.com/notifications | jq '. | length'`
-
-if [[ "$count" != "0" ]]; then
-    echo '{"text":'$count',"tooltip":"$tooltip","class":"$class"}'
-fi
